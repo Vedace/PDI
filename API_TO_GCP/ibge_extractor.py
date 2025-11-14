4edd9403dc40e4445e83d4de4595b39e067372b5
Ernesto <vedace91@gmail.com>
2025-11-13 23:18:10 -0300

    Adicionando extractor de dados IBGE

---
+++ API_TO_GCP/ibge_extractor.py
@@ -0,0 +1,12 @@
+import requests
+import pandas as pd 
+
+ibge = 'https://servicodados.ibge.gov.br/api/v1/localidades/distritos'
+
+dados_ibge = requests.get (ibge)
+
+ibge_json = dados_ibge.json()
+
+df_ibge = pd.json_normalize(ibge_json, sep = '_')
+
+df_ibge.to_parquet("ibge.parquet", index=False) # modificar para cloud storage
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PDI
 Repositorio dos proyectos de desenvolvimento pessoal
+
+API_TO_GCP - contem os extractores de dados das APIs
+
