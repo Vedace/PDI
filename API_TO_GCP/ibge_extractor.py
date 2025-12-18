f56c05e7bf1d2c651c4d3399519b904e127ccbf2
Ernesto <vedace91@gmail.com>
2025-12-17 22:58:04 -0300

    Fix

--- API_TO_GCP/ibge_extractor.py
+++ API_TO_GCP/ibge_extractor.py
@@ -1,6 +1,10 @@
 import requests
-import pandas as pd 
+import pandas as pd
+from datetime import datetime, timedelta
 
+#Creando un timestamp actual y otro d-1 
+timestamp_atual = datetime.now().strftime("%Y%m%d_%H%M%S")
+timestamp_ontem = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d_%H%M%S")
 
 ibge = 'https://servicodados.ibge.gov.br/api/v1/localidades/municipios?view=nivelado'
 
@@ -16,6 +20,14 @@ ibge_columns = ["id_municipio", "nome_municipio","id_microrregiao","nome_microrr
 
 df_ibge.columns = ibge_columns
 
+df_ibge_len = len(df_ibge)
+df_ibge_mitade = df_ibge_len // 2 
+
+df_ibge1 = df.iloc[:df_ibge_mitade]
+df_ibge2 = df.iloc[df_ibge_mitade:]
+
 print(df_ibge.head())
 
-df_ibge.to_parquet("ibge.parquet", index=False) # modificar para cloud storage
+df_ibge.to_parquet(f"ibge_{timestamp_ontem}.parquet", index=False)
+df_ibge1.to_parquet(f"ibge1_{timestamp_atual}.parquet", index=False)
+df_ibge2.to_parquet(f"ibge2_{timestamp_atual}.parquet", index=False) # modificar para cloud storage
